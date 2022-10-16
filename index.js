20457fd894332ad744633b774f4f49fe930cd586
fabrici2052s <115957585+fabrici2052s@users.noreply.github.com>
2022-10-16 18:24:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const yesbtn = document.querySelector("#yesbtn");
+
+yesbtn.addEventListener("click",function () {
+    alert("sabia que dirias que si <3 xd")
+});
+
+
+const nobtn = document.querySelector("#nobtn");
+
+nobtn.addEventListener("click",function () {
+    alert("te endiendo....lo siento mucho por no averte esperado...gracias por todo mi chinita te amo y te amare para siempre <3")
+});
+
